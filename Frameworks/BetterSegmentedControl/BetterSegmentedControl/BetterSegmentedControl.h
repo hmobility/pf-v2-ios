4183aac3bf45f4aa3f1387378c59dd88183dbf89
mkjwa <mkjwa@hancom.com>
2020-02-05 09:10:35 +0900

    - BetterSegmentedControl 프로젝트 추가

---
+++ Frameworks/BetterSegmentedControl/BetterSegmentedControl/BetterSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  BetterSegmentedControl.h
+//  BetterSegmentedControl
+//
+//  Created by PlankFish on 2020/02/03.
+//  Copyright © 2020 PlankFish. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BetterSegmentedControl.
+FOUNDATION_EXPORT double BetterSegmentedControlVersionNumber;
+
+//! Project version string for BetterSegmentedControl.
+FOUNDATION_EXPORT const unsigned char BetterSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetterSegmentedControl/PublicHeader.h>
+
+
