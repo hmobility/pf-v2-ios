8c664d89cacc3ad7096b74af348b79e6757bdd44
Cho Myoung Hyoun <>
2020-02-17 11:34:50 +0900

    MapView+Rx : Edit radius to original

--- ParkingFriends/02-Common/RxSupport/Extension/MapView+Rx.swift
+++ ParkingFriends/02-Common/RxSupport/Extension/MapView+Rx.swift
@@ -36,6 +36,7 @@ extension Reactive where Base:  NMFMapView {
         }
     }
     
+    /*
     public var radius:Double {
         get {
             // Add by Rao
@@ -49,6 +50,17 @@ extension Reactive where Base:  NMFMapView {
             
             
             return (metersPerPixel * Double(width)) / 1000.0
+        }
+    }
+     */
+    
+    public var radius:Double {
+        get {
+            let width = UIScreen.main.nativeBounds.width / 2
+            let metersPerPixel = self.base.projection.metersPerPixel()
+            let scale = UIScreen.main.nativeScale           // To divide by Physical Screen Scale
+            
+            return (((metersPerPixel * Double(width)) / 1000) / Double(scale))
         }
     }
     // MRAK: - Delegate
